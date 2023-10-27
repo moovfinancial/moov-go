d623f9057e53754299a34bc9bea057bcdadaec53
Wade Arnold <wade@wadearnold.com>
2023-10-27 17:48:56 -0600

    Create client to get server side token

--- client/client.go
+++ client/client.go
@@ -1,4 +1,69 @@
-package client
+package moov
+
+import (
+	"encoding/json"
+	"fmt"
+	"io"
+	"log"
+	"net/http"
+	"net/url"
+)
 
 // New create4s a new Moov client with the appropriate secret key.
 //func New(private)
+
+type Client struct {
+	KeyPublic string
+	KeySecret string
+	Domain    string
+}
+
+type ClientCredentialsGrantToAccessTokenResponse struct {
+	AccessToken  string `json:"access_token"`
+	RefreshToken string `json:"refresh_token"`
+	TokenType    string `json:"token_type"`
+	ExpiresIn    int32  `json:"expires_in"`
+	Scope        string `json:"scope"`
+}
+
+// BasicAuth calls
+func (c Client) BasicAuthToken() (ClientCredentialsGrantToAccessTokenResponse, error) {
+	token := ClientCredentialsGrantToAccessTokenResponse{}
+	if c.KeyPublic == "" || c.KeySecret == "" {
+		// Make error for token's not set.
+		return token, fmt.Errorf("API Keys are not set")
+	}
+
+	params := url.Values{}
+	params.Add("grant_type", "client_credentials")
+	params.Add("scope", "/accounts.write")
+
+	req, err := http.NewRequest("POST", "https://api.moov.io/oauth2/token?"+params.Encode(), nil)
+	if err != nil {
+		// Todo: return an error
+		log.Fatal(err)
+	}
+
+	req.Header.Set("Accept", "application/json")
+	req.Header.Set("Content-Type", "application/x-www-form-urlencoded")
+	req.SetBasicAuth(c.KeyPublic, c.KeySecret)
+
+	client := &http.Client{}
+	resp, err := client.Do(req)
+	if err != nil {
+		// Todo: return an error
+		log.Fatal(err)
+	}
+
+	resBody, err := io.ReadAll(resp.Body)
+	if err != nil {
+		// Todo: return an error
+		log.Fatal(err)
+	}
+
+	if err := json.Unmarshal(resBody, &token); err != nil { // Parse []byte to go struct pointer
+		// Todo: return an error
+		log.Fatal("Can not unmarshal JSON")
+	}
+	return token, nil
+}
--- main.go
+++ main.go
@@ -1,63 +1,20 @@
 package main
 
 import (
-	b64 "encoding/base64"
-	"encoding/json"
-	"fmt"
-	"io"
 	"log"
-	"net/http"
-	"net/url"
+	moov "moov-golang/client"
 )
-
-type ClientCredentialsGrantToAccessTokenResponse struct {
-	AccessToken  string `json:"access_token`
-	RefreshToken string `json:"access_token`
-	TokenType    string `json:"token_type`
-	ExpiresIn    int32  `json:"expires_in`
-	Scope        string `json:"scope"`
-}
 
 func main() {
 
 	// Setup Moov client
 	// Generate API keys from the Moov Dashboard. Sandbox keys are not compatable with production.
+	moov := new(moov.Client)
+	moov.KeyPublic = "Qo0j0ChSalMKmRI_"
+	moov.KeySecret = "nbyCvG0r0MpM2gxkzo2u2G70jr6ZtFdm"
 
-	KeyPublic := "Qo0j0ChSalMKmRI_"
-	KeySecret := "nbyCvG0r0MpM2gxkzo2u2G70jr6ZtFdm"
+	accessToken, _ := moov.BasicAuthToken()
 
-	key := b64.StdEncoding.EncodeToString([]byte(KeyPublic + ":" + KeySecret))
-	log.Println(key)
-
-	params := url.Values{}
-	params.Add("grant_type", "client_credentials")
-	params.Add("scope", "/accounts.write")
-
-	req, err := http.NewRequest("POST", "https://api.moov.io/oauth2/token?"+params.Encode(), nil)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	req.Header.Set("Accept", "application/json")
-	req.Header.Set("Content-Type", "application/x-www-form-urlencoded")
-	req.SetBasicAuth(KeyPublic, KeySecret)
-
-	client := &http.Client{}
-	resp, err := client.Do(req)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	resBody, err := io.ReadAll(resp.Body)
-	if err != nil {
-		log.Fatal(err)
-	}
-	log.Printf("%s", resBody)
-
-	tokenResponse := ClientCredentialsGrantToAccessTokenResponse{}
-	if err := json.Unmarshal(resBody, &tokenResponse); err != nil { // Parse []byte to go struct pointer
-		fmt.Println("Can not unmarshal JSON")
-	}
-	log.Printf("tokenResponse: %s", tokenResponse.AccessToken)
+	log.Println(accessToken.AccessToken)
 
 }
