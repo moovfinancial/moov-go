386d5867a7df7f8ab14e9a205be2b4203407cc95
wadearnold <wade@moov.io>
2023-11-13 17:13:50 -0700

    Add ApplePay model

--- pkg/applePay.go
+++ pkg/applePay.go
@@ -1,5 +1,7 @@
 package moov
 
+import "time"
+
 type ApplePay struct {
 	Brand           string     `json:"brand,omitempty"`
 	CardType        string     `json:"cardType,omitempty"`
@@ -8,3 +10,31 @@ type ApplePay struct {
 	Expiration      Expiration `json:"expiration,omitempty"`
 	DynamicLastFour string     `json:"dynamicLastFour,omitempty"`
 }
+
+type ApplePayDomains struct {
+	DisplayName string   `json:"displayName,omitempty"`
+	Domains     []string `json:"domains,omitempty"`
+}
+
+type ApplePayDomainsResponse struct {
+	AccountID   string    `json:"accountID,omitempty"`
+	DisplayName string    `json:"displayName,omitempty"`
+	Domains     []string  `json:"domains,omitempty"`
+	CreatedOn   time.Time `json:"createdOn,omitempty"`
+	UpdatedOn   time.Time `json:"updatedOn,omitempty"`
+}
+
+// CreateApplePayDomain creates a new Apple Pay domain for the given customer account
+// https://docs.moov.io/api/#tag/Cards/operation/registerApplePayMerchantDomains
+
+// UpdateApplePayDomain updates an existing Apple Pay domain for the given customer account
+// https://docs.moov.io/api/#tag/Cards/operation/updateApplePayMerchantDomains
+
+// GetApplePayDomain retrieves an Apple Pay domain for the given customer account
+// https://docs.moov.io/api/#tag/Cards/operation/getApplePayMerchantDomains
+
+// CreateApplePaySession creates a new Apple Pay session for the given customer account
+// https://docs.moov.io/api/#tag/Cards/operation/getApplePayMerchantDomains
+
+// ApplePayToken creates a new Apple Pay token for the given customer account
+// https://docs.moov.io/api/#tag/Cards/operation/getApplePayMerchantDomains
