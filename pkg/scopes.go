6f96a99d6797cf06e1729260ca7831a3072cdb9e
JJ Philipp <jj@moov.io>
2024-01-22 16:44:56 -0600

    Cleaned up the scopes API to make it easier and typed

---
+++ pkg/scopes.go
@@ -0,0 +1,146 @@
+package moov
+
+import (
+	"fmt"
+	"strings"
+)
+
+// Setup namespacing so when you use these scopes its `moov.Scopes.AccountsRead()`
+var Scopes scopeList = scopeList{}
+
+// Used for listing the accounts the facilitator has been connected to.
+func (sl *scopeList) AccountsRead() ScopeBuilder {
+	return appendScope("/accounts.read")
+}
+
+// Used for creating a new account thats auto-connected to the facilitator.
+func (sl *scopeList) AccountsWrite() ScopeBuilder {
+	return appendScope("/accounts.write")
+}
+
+func (sl *scopeList) BankAccountsRead(accountID string) ScopeBuilder {
+	return appendScope("/accounts/%s/bank-accounts.read", accountID)
+}
+
+func (sl *scopeList) BankAccountsWrite(accountID string) ScopeBuilder {
+	return appendScope("/accounts/%s/bank-accounts.write", accountID)
+}
+
+func (sl *scopeList) CapabilitiesRead(accountID string) ScopeBuilder {
+	return appendScope("/accounts/%s/capabilities.read", accountID)
+}
+
+func (sl *scopeList) CapabilitiesWrite(accountID string) ScopeBuilder {
+	return appendScope("/accounts/%s/capabilities.write", accountID)
+}
+
+func (sl *scopeList) CardsRead(accountID string) ScopeBuilder {
+	return appendScope("/accounts/%s/cards.read", accountID)
+}
+
+func (sl *scopeList) CardsWrite(accountID string) ScopeBuilder {
+	return appendScope("/accounts/%s/cards.write", accountID)
+}
+
+func (sl *scopeList) IssuedCardsRead(accountID string) ScopeBuilder {
+	return appendScope("/accounts/%s/issued-cards.read", accountID)
+}
+
+// Allows for the creation of a new issued card
+func (sl *scopeList) IssuedCardsWrite(accountID string) ScopeBuilder {
+	return appendScope("/accounts/%s/issued-cards.write", accountID)
+}
+
+// WARNING: Will return PCI data only allowed use case is by the customer directly to moov otherwise your business will require a PCI audit.
+func (sl *scopeList) IssuedCardsReadSecure(accountID string) ScopeBuilder {
+	return appendScope("/accounts/%s/issued-cards.read-secure", accountID)
+}
+
+// Give to the registered ApplePay merchants to allow them manage their ApplePay integration.
+func (sl *scopeList) ApplePayMerchantRead(accountID string) ScopeBuilder {
+	return appendScope("/accounts/%s/apple-pay.read", accountID)
+}
+
+// Give to the registered ApplePay merchants to allow them manage their ApplePay integration.
+func (sl *scopeList) ApplePayMerchantWrite(accountID string) ScopeBuilder {
+	return appendScope("/accounts/%s/apple-pay.write", accountID)
+}
+
+func (sl *scopeList) ApplePayWrite(accountID string) ScopeBuilder {
+	return appendScope("/accounts/%s/apple-pay.write", accountID)
+}
+
+func (sl *scopeList) AccountProfileRead(accountID string) ScopeBuilder {
+	return appendScope("/accounts/%s/profile.read", accountID)
+}
+
+func (sl *scopeList) AccountProfileWrite(accountID string) ScopeBuilder {
+	return appendScope("/accounts/%s/profile.write", accountID)
+}
+
+func (sl *scopeList) AccountRepresentativesRead(accountID string) ScopeBuilder {
+	return appendScope("/accounts/%s/representatives.read", accountID)
+}
+
+func (sl *scopeList) AccountRepresentativesWrite(accountID string) ScopeBuilder {
+	return appendScope("/accounts/%s/representatives.write", accountID)
+}
+
+func (sl *scopeList) FilesRead(accountID string) ScopeBuilder {
+	return appendScope("/accounts/%s/files.read", accountID)
+}
+
+func (sl *scopeList) FilesWrite(accountID string) ScopeBuilder {
+	return appendScope("/accounts/%s/files.write", accountID)
+}
+
+func (sl *scopeList) PaymentMethodsRead(accountID string) ScopeBuilder {
+	return appendScope("/accounts/%s/payment-methods.read", accountID)
+}
+
+func (sl *scopeList) WalletsRead(accountID string) ScopeBuilder {
+	return appendScope("/accounts/%s/wallets.read", accountID)
+}
+
+func (sl *scopeList) Ping() ScopeBuilder {
+	return appendScope("/ping.read")
+}
+
+func (sl *scopeList) Fed() ScopeBuilder {
+	return appendScope("/fed.read")
+}
+
+func (sl *scopeList) ProfileEnrichment() ScopeBuilder {
+	return appendScope("/profile-enrichment.read")
+}
+
+// Boilerplate for setting the above.
+
+type scopeList struct{}
+
+type ScopeBuilder func(sb *scopeBuilder) error
+
+type scopeBuilder struct {
+	scopes []string
+}
+
+func buildScopes(scopes ...ScopeBuilder) (string, error) {
+	sb := &scopeBuilder{
+		scopes: []string{},
+	}
+
+	for _, scp := range scopes {
+		if err := scp(sb); err != nil {
+			return "", err
+		}
+	}
+
+	return strings.Join(sb.scopes, " "), nil
+}
+
+func appendScope(scope string, args ...any) ScopeBuilder {
+	return func(sb *scopeBuilder) error {
+		sb.scopes = append(sb.scopes, fmt.Sprintf(scope, args...))
+		return nil
+	}
+}
--- pkg/token.go
+++ pkg/token.go
@@ -3,36 +3,6 @@ package moov
 import (
 	"context"
 	"net/http"
-	"strings"
-)
-
-const (
-	SCOPE_ACCOUNTS_READ  = "/accounts.read"
-	SCOPE_ACCOUNTS_WRITE = "/accounts.write"
-
-	// TODO: Add the rest of the scopes https://docs.moov.io/guides/developer-tools/scopes/
-	SCOPE_BANK_ACCOUNT_READ       = "/accounts/{accountID}/bank-accounts.read"
-	SCOPE_BANK_ACCOUNT_WRITE      = "/accounts/{accountID}/bank-accounts.write"
-	SCOPE_CARDS_READ              = "/accounts/{accountID}/cards.read"
-	SCOPE_CARDS_WRITE             = "/accounts/{accountID}/cards.write"
-	SCOPE_CAPABILITIES_READ       = "/accounts/{accountID}/capabilities.read"
-	SCOPE_CAPABILITIES_WRITE      = "/accounts/{accountID}/capabilities.write"
-	SCOPE_DOCUMENTS_READ          = "/accounts/{accountID}/documents.read"
-	SCOPE_DOCUMENTS_WRITE         = "/accounts/{accountID}/documents.write"
-	SCOPE_FILES_READ              = "/accounts/{accountID}/files.read"
-	SCOPE_FILES_WRITE             = "/accounts/{accountID}/files.write"
-	SCOPE_PAYMENT_METHODS_READ    = "/accounts/{accountID}/payment-methods.read"
-	SCOPE_PAYMENT_METHODS_WRITE   = "/accounts/{accountID}/payment-methods.write"
-	SCOPE_PROFILE_READ            = "/accounts/{accountID}/profile.read"
-	SCOPE_PROFILE_WRITE           = "/accounts/{accountID}/profile.write"
-	SCOPE_PREFILE_ENRICHMENT_READ = "/accounts/{accountID}/prefile-enrichment.read"
-	SCOPE_REPRESENTATIVES_READ    = "/accounts/{accountID}/representatives.read"
-	SCOPE_REPRESENTATIVES_WRITE   = "/accounts/{accountID}/representatives.write"
-	SCOPE_TRANSFERS_READ          = "/accounts/{accountID}/transfers.read"
-	SCOPE_TRANSFERS_WRITE         = "/accounts/{accountID}/transfers.write"
-	SCOPE_WALLETS_READ            = "/accounts/{accountID}/wallets.read"
-	SCOPE_FED_READ                = "/fed.read"
-	SCOPE_PING_READ               = "/ping.read"
 )
 
 type AccessTokenRequest struct {
@@ -47,6 +17,20 @@ type AccessTokenRequest struct {
 	Audience string `json:"audience,omitempty"`
 	// String passed to the authorization server to gain access to the system
 	RefreshToken string `json:"refresh_token,omitempty"`
+}
+
+func (atr *AccessTokenRequest) WithScopes(scopes ...ScopeBuilder) error {
+	scp, err := buildScopes(scopes...)
+	if err != nil {
+		return err
+	}
+
+	if atr.Scope != "" {
+		scp = scp + " " + atr.Scope
+	}
+
+	atr.Scope = scp
+	return nil
 }
 
 // AccessTokenResponse Possible responses that can be made to the /token endpoints. The type of response returned here will mirror the type of the request
@@ -64,7 +48,11 @@ type AccessTokenResponse struct {
 }
 
 // Makes the call for creating the access tokens
-func (c *Client) AccessToken(ctx context.Context, tokenReq AccessTokenRequest) (*AccessTokenResponse, error) {
+func (c *Client) AccessToken(ctx context.Context, tokenReq AccessTokenRequest, scopes ...ScopeBuilder) (*AccessTokenResponse, error) {
+	if err := tokenReq.WithScopes(scopes...); err != nil {
+		return nil, err
+	}
+
 	resp, err := c.CallHttp(ctx,
 		Endpoint(http.MethodPost, "/oauth2/token"),
 		JsonBody(tokenReq))
@@ -96,8 +84,7 @@ func (c *Client) PingAccessToken(ctx context.Context) (*AccessTokenResponse, err
 		GrantType:    "client_credentials",
 		ClientId:     &c.Credentials.PublicKey,
 		ClientSecret: &c.Credentials.SecretKey,
-		Scope:        SCOPE_PING_READ,
-	})
+	}, Scopes.Ping())
 }
 
 // Creates a access token that can only be used for creating a new account thats connected to the client.
@@ -108,21 +95,16 @@ func (c *Client) AccountCreationToken(ctx context.Context) (*AccessTokenResponse
 		GrantType:    "client_credentials",
 		ClientId:     &c.Credentials.PublicKey,
 		ClientSecret: &c.Credentials.SecretKey,
-		Scope:        SCOPE_ACCOUNTS_WRITE,
-	})
+	}, Scopes.AccountsWrite())
 }
 
 // Creates an access token to access a connected account.
 // This allows for a browser to access an account directly against Moov servers without that needing to send sensitive
 // information through the clients backend services.
-func (c *Client) AccountAccessToken(ctx context.Context, accountID string, scopes []string) (*AccessTokenResponse, error) {
-	renderedScopes := strings.Join(scopes, " ")
-	renderedScopes = strings.ReplaceAll(renderedScopes, "{accountID}", accountID)
-
+func (c *Client) AccountAccessToken(ctx context.Context, accountID string, scopes ...ScopeBuilder) (*AccessTokenResponse, error) {
 	return c.AccessToken(ctx, AccessTokenRequest{
 		GrantType:    "client_credentials",
 		ClientId:     &c.Credentials.PublicKey,
 		ClientSecret: &c.Credentials.SecretKey,
-		Scope:        renderedScopes,
-	})
+	}, scopes...)
 }
