9a4d4e8bc691d947d43ae39c8f9dea3a26d693cf
Adam Shannon <adamkshannon@gmail.com>
2024-02-23 12:48:34 -0600

    examples: convert to tests

--- examples/example_checkout_test.go
+++ examples/example_checkout_test.go
@@ -1,40 +1,32 @@
-package main
+package examples
 
 import (
 	"context"
-	"fmt"
+	"testing"
 
 	"github.com/moovfinancial/moov-go/pkg/moov"
+
+	"github.com/stretchr/testify/require"
 )
 
-func main() {
-	fmt.Println("Moov SDK Examples")
-
+func TestCheckout(t *testing.T) {
 	// Create a new moov client
-	mc, err := moov.NewClient()
-	if err != nil {
-		// Exit with invalid credentials
-		fmt.Println(err)
-		return
-	}
+	mc, err := moov.NewClient(moov.WithCredentials(moov.CredentialsFromEnv()))
+	require.NoError(t, err)
 
 	// Create a new context or use an existing one
 	ctx := context.Background()
 
 	// Ping the server to check credentials
 	err = mc.Ping(ctx)
-	if err != nil {
-		fmt.Println(err)
-	}
+	require.NoError(t, err)
 
 	// Create access token to instantiate a moov.js drop for creating an account
 	token, err := mc.AccountCreationToken(ctx)
-	if err != nil {
-		fmt.Println(err)
-	}
+	require.NoError(t, err)
 
 	// load this into a moov.js drop to create an account
-	fmt.Printf("AccountCreationToken: %s \n", token.AccessToken)
+	t.Logf("AccountCreationToken=%s", token.AccessToken)
 
 	// Create a new moov account server side
 	account := moov.Account{
@@ -54,51 +46,44 @@ func main() {
 		},
 	}
 	completedAccount, _, err := mc.CreateAccount(ctx, account)
-	if err != nil {
-		fmt.Println(err)
-	}
-	fmt.Printf("AccountID: %s \n", completedAccount.AccountID)
+	require.NoError(t, err)
+
+	t.Logf("AccountID=%s", completedAccount.AccountID)
 
 	// You can search for all existing accounts with a variety of query parameter's. We will use this account as the destination to receive money.
 	accounts, err := mc.ListAccounts(ctx, moov.WithAccountName("Lincoln National Corporation"), moov.WithAccountCount(10))
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
+	require.NoError(t, err)
+
 	var lincolnAccount moov.Account
 	for _, account := range accounts {
-		fmt.Printf("AccountID: %s \t Display Name: %s \n", account.AccountID, account.DisplayName)
+		t.Logf("AccountID=%s \t Display Name=%s", account.AccountID, account.DisplayName)
 		if account.DisplayName == "Lincoln National Corporation" {
 			lincolnAccount = account
 		}
 	}
 	// Get the paymentMethodID for the Lincoln National wallet
 	lincolnPaymentMethods, err := mc.ListPaymentMethods(ctx, lincolnAccount.AccountID, moov.WithPaymentMethodType(("moov-wallet")))
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
+	require.NoError(t, err)
+
 	// As of the writing of this example, every account has one and only one wallet
 	if len(lincolnPaymentMethods) == 0 {
-		fmt.Println("No wallet found for Lincoln National Corporation")
-		return
+		t.Fatal("No wallet found for Lincoln National Corporation")
 	}
 	lincolnWallet := lincolnPaymentMethods[0]
 
-	fmt.Printf("Lincoln AccountID: %s \t PaymentMethodID: %s\n", lincolnAccount.AccountID, lincolnWallet.PaymentMethodID)
+	t.Logf("Lincoln AccountID=%s \t PaymentMethodID=%s", lincolnAccount.AccountID, lincolnWallet.PaymentMethodID)
 
 	// To link a card, apple pay, or bank account to an account you can use a Moov drop to stay out of PCI scope.
 	// Create a scoped access token for the account we just created. This token can link every payment type from Moov.js
 	token, err = mc.AccessToken(ctx, moov.Scopes.CardsWrite(completedAccount.AccountID),
 		moov.Scopes.BankAccountsWrite(completedAccount.AccountID),
 		moov.Scopes.ApplePayWrite(completedAccount.AccountID))
-	if err != nil {
-		fmt.Println(err)
-	}
+	require.NoError(t, err)
+
 	// Use this token in the card or bank account linking Drop.
 	// Token's allow the browser to link cards and bank accounts to the account directly with Moov.
 	//fmt.Println(completedAccount.AccountID) // cardInput.AccountID
-	fmt.Println(token.AccessToken) // cardInput.AccessToken
+	t.Logf("access token: %v", token.AccessToken) // cardInput.AccessToken
 
 	// The drop's onSuccess event for the card linking drop returns a Card object the cardID and paymentMethodID
 	// The paymentMethodID is used to transfer money from the card to the moov wallet and needs to be sent to the server
@@ -117,10 +102,8 @@ func main() {
 		},
 	}
 	linkCard, err := mc.CreateCard(ctx, completedAccount.AccountID, card)
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
+	require.NoError(t, err)
+
 	// The same card can be used for card-payment, push-to-card or pull-from-card and each of their own paymentMethodID
 	var cardPaymentMethodID string
 	for _, pm := range linkCard.PaymentMethods {
@@ -128,15 +111,15 @@ func main() {
 			cardPaymentMethodID = pm.PaymentMethodID
 		}
 	}
-	fmt.Printf("PaymentMethodID: %s \t lastFourCardNumber: %s \t brand: %s \n", cardPaymentMethodID, linkCard.LastFourCardNumber, linkCard.Brand)
+
+	t.Logf("PaymentMethodID=%s \t lastFourCardNumber=%s \t brand=%s", cardPaymentMethodID, linkCard.LastFourCardNumber, linkCard.Brand)
 
 	// An existing linked card can be used to transfer money to the moov wallet by finding it's paymentMethodID
 	paymentMethods, err := mc.ListPaymentMethods(ctx, completedAccount.AccountID, moov.WithPaymentMethodType("card-payment"))
-	if err != nil {
-		fmt.Println(err)
-	}
+	require.NoError(t, err)
+
 	for _, paymentMethod := range paymentMethods {
-		fmt.Printf("PaymentMethodID: %s \t lastFourCardNumber: %s \t PaymentMethodType: %s \n", paymentMethod.PaymentMethodID, paymentMethod.Card.LastFourCardNumber, paymentMethod.PaymentMethodType)
+		t.Logf("PaymentMethodID=%s \t lastFourCardNumber=%s \t PaymentMethodType=%s", paymentMethod.PaymentMethodID, paymentMethod.Card.LastFourCardNumber, paymentMethod.PaymentMethodType)
 	}
 
 	// We showed three ways to get a PaymentMethodID to transfer money from a card to the moov wallet.
@@ -171,17 +154,13 @@ func main() {
 		FacilitatorFee: facilitatorFee,
 		Description:    description,
 	}, moov.WithTransferWaitForRailResponse())
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
-	fmt.Printf("TransferID: %s \t Status: %s \n", completedTransfer.TransferID, completedTransfer.Status)
+	require.NoError(t, err)
+
+	t.Logf("TransferID=%s \t Status=%s \n", completedTransfer.TransferID, completedTransfer.Status)
 
 	// We can check the status of the transfer. More important for batched ACH transactions
 	transferStatus, err := mc.GetTransfer(ctx, completedTransfer.TransferID)
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
-	fmt.Printf("TransferID: %s \t Status: %s \n", transferStatus.TransferID, transferStatus.Status)
+	require.NoError(t, err)
+
+	t.Logf("TransferID=%s \t Status=%s \n", transferStatus.TransferID, transferStatus.Status)
 }
