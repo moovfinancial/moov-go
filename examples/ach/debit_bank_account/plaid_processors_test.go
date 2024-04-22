95492c6f26ed6817227ecb64afb7bcddabbad021
Adam Shannon <adamkshannon@gmail.com>
2024-04-22 11:59:14 -0500

    examples/ach: generate plaid processor token on the fly

--- examples/ach/debit_bank_account/plaid_processors_test.go
+++ examples/ach/debit_bank_account/plaid_processors_test.go
@@ -1,12 +1,18 @@
 package debit_bank_account
 
 import (
+	"bytes"
 	"context"
+	"encoding/json"
+	"io"
+	"net/http"
 	"os"
+	"strings"
 	"testing"
 
-	"github.com/go-faker/faker/v4"
 	"github.com/moovfinancial/moov-go/pkg/moov"
+
+	"github.com/go-faker/faker/v4"
 	"github.com/stretchr/testify/require"
 )
 
@@ -53,8 +59,14 @@ func TestPlaidProcessorExample(t *testing.T) {
 	//
 	// See https://plaid.com/docs/auth/partnerships/moov/ for Plaid's documentation
 	// See https://docs.moov.io/guides/sources/bank-accounts/plaid/ for Moov's documentation
+	plaidProcessorToken := strings.TrimSpace(os.Getenv("PLAID_PROCESSOR_TOKEN"))
+	if plaidProcessorToken == "" {
+		plaidProcessorToken = createLinkToken(t)
+	}
+	require.NotEmpty(t, plaidProcessorToken)
+
 	plaid := moov.PlaidRequest{
-		Token: os.Getenv("PLAID_PROCESSOR_TOKEN"),
+		Token: plaidProcessorToken,
 	}
 	bankAccount, err := mc.CreateBankAccount(ctx, account.AccountID, moov.WithPlaid(plaid), moov.WaitForPaymentMethod())
 	require.NoError(t, err)
@@ -76,7 +88,6 @@ func TestPlaidProcessorExample(t *testing.T) {
 	// destination Moov wallet ("moov-wallet" payment method).
 	paymentMethods, err = mc.ListPaymentMethods(ctx, destinationAccountID, moov.WithPaymentMethodType("moov-wallet"))
 	require.NoError(t, err)
-
 	require.Len(t, paymentMethods, 1)
 
 	// This is the destination payment method (Moov wallet)
@@ -107,4 +118,60 @@ func TestPlaidProcessorExample(t *testing.T) {
 	t.Logf("Amount: %#v", completedTransfer.Amount)
 	t.Logf("Status: %v", completedTransfer.Status)
 	t.Logf("CreatedOn: %v", completedTransfer.CreatedOn)
+}
+
+type plaidLinkTokenRequest struct {
+	ClientID   string `json:"client_id"`
+	Secret     string `json:"secret"`
+	ClientName string `json:"client_name"`
+	User       struct {
+		ClientUserID string `json:"client_user_id"`
+	} `json:"user"`
+	Products     []string `json:"products"`
+	CountryCodes []string `json:"country_codes"`
+	Language     string   `json:"language"`
+	Webhook      string   `json:"webhook"`
+	RedirectURI  string   `json:"redirect_uri"`
+}
+
+type plaidLinkTokenResponse struct {
+	LinkToken string `json:"link_token"`
+}
+
+func createLinkToken(t testing.TB) string {
+	t.Helper()
+
+	linkToken := plaidLinkTokenRequest{
+		ClientID:     os.Getenv("PLAID_CLIENT_ID"),
+		Secret:       os.Getenv("PLAID_SECRET"),
+		ClientName:   "Moov Debit BankAccount Example",
+		Products:     []string{"auth"},
+		CountryCodes: []string{"US"},
+		Language:     "en",
+		Webhook:      "https://plaid-processor-test.moov.io",
+		RedirectURI:  "https://local.moov.io",
+	}
+	linkToken.User.ClientUserID = "4c7ab47a-487a-4c91-92a7-7fda891b2bb0"
+
+	var buf bytes.Buffer
+	json.NewEncoder(&buf).Encode(linkToken)
+
+	req, err := http.NewRequest("POST", "https://sandbox.plaid.com/link/token/create", &buf)
+	require.NoError(t, err)
+	req.Header.Set("Content-Type", "application/json")
+
+	resp, err := http.DefaultClient.Do(req)
+	require.NoError(t, err)
+	t.Cleanup(func() { resp.Body.Close() })
+
+	bs, _ := io.ReadAll(resp.Body)
+
+	var response plaidLinkTokenResponse
+	json.NewDecoder(bytes.NewReader(bs)).Decode(&response)
+
+	if response.LinkToken == "" {
+		t.Log("Plaid Link Token Response")
+		t.Logf("%v", string(bs))
+	}
+	return response.LinkToken
 }
