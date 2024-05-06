4ab23f3d6b3678a56f1d600e71cb024bb80fde65
droachmoov <david@moov.io>
2024-05-06 08:59:07 -0700

    [COR-3107] Added function headers

--- pkg/moov/underwriting_api.go
+++ pkg/moov/underwriting_api.go
@@ -5,6 +5,9 @@ import (
 	"net/http"
 )
 
+// UpsertUnderwriting adds or updates underwriting information for the given account.
+// The account must have a description and an MCC set to create underwriting.
+// Returns the underwriting information for the account.
 func (c Client) UpsertUnderwriting(ctx context.Context, accountID string, underwriting UpdateUnderwriting) (*Underwriting, error) {
 	resp, err := c.CallHttp(ctx,
 		Endpoint(http.MethodPut, pathUnderwriting, accountID),
@@ -17,6 +20,7 @@ func (c Client) UpsertUnderwriting(ctx context.Context, accountID string, underw
 	return CompletedObjectOrError[Underwriting](resp)
 }
 
+// GetUnderwriting returns the underwriting information for the given account.
 func (c Client) GetUnderwriting(ctx context.Context, accountID string) (*Underwriting, error) {
 	resp, err := c.CallHttp(ctx,
 		Endpoint(http.MethodGet, pathUnderwriting, accountID),
