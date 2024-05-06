1b79c712bd21c6db46b203df498b85d419939ac3
droachmoov <david@moov.io>
2024-05-06 11:24:21 -0700

    [COR-3107] updated underwriting

--- pkg/moov/underwriting_test.go
+++ pkg/moov/underwriting_test.go
@@ -17,15 +17,36 @@ func TestUpsertUnderwriting(t *testing.T) {
 		AverageTransactionSize:          1_000,
 		MaxTransactionSize:              500,
 		AverageMonthlyTransactionVolume: 10_000,
+		Fulfillment: moov.Fulfillment{
+			HasPhysicalGoods:     true,
+			IsShippingProduct:    true,
+			ShipmentDurationDays: 3,
+			ReturnPolicy:         moov.WITHIN_THIRTY_DAYS,
+		},
+		CardVolumeDistribution: moov.CardVolumeDistribution{
+			EcommercePercentage:     50,
+			CardPresentPercentage:   30,
+			MailOrPhonePercentage:   10,
+			DebtRepaymentPercentage: 10,
+		},
+		VolumeByCustomerType: moov.VolumeByCustomerType{
+			BusinessToBusinessPercentage: 60,
+			ConsumerToBusinessPercentage: 40,
+		},
 	}
 
 	t.Run("insert", func(t *testing.T) {
 		actual, err := mc.UpsertUnderwriting(context.Background(), account.AccountID, create)
 
 		NoResponseError(t, err)
+		require.NotNil(t, actual)
 		require.Equal(t, create.AverageTransactionSize, actual.AverageTransactionSize)
 		require.Equal(t, create.MaxTransactionSize, actual.MaxTransactionSize)
 		require.Equal(t, create.AverageMonthlyTransactionVolume, actual.AverageMonthlyTransactionVolume)
+		//todo: comment in once the flag is enabled
+		// require.Equal(t, create.Fulfillment, actual.Fulfillment)
+		// require.Equal(t, create.CardVolumeDistribution, actual.CardVolumeDistribution)
+		// require.Equal(t, create.VolumeByCustomerType, actual.VolumeByCustomerType)
 		require.Equal(t, moov.UnderwritingStatusNotRequested, actual.Status)
 	})
 
@@ -33,9 +54,14 @@ func TestUpsertUnderwriting(t *testing.T) {
 		actual, err := mc.GetUnderwriting(context.Background(), account.AccountID)
 
 		NoResponseError(t, err)
+		require.NotNil(t, actual)
 		require.Equal(t, create.AverageTransactionSize, actual.AverageTransactionSize)
 		require.Equal(t, create.MaxTransactionSize, actual.MaxTransactionSize)
 		require.Equal(t, create.AverageMonthlyTransactionVolume, actual.AverageMonthlyTransactionVolume)
+		//todo: comment in once the flag is enabled
+		// require.Equal(t, create.Fulfillment, actual.Fulfillment)
+		// require.Equal(t, create.CardVolumeDistribution, actual.CardVolumeDistribution)
+		// require.Equal(t, create.VolumeByCustomerType, actual.VolumeByCustomerType)
 		require.Equal(t, moov.UnderwritingStatusNotRequested, actual.Status)
 	})
 
@@ -43,15 +69,36 @@ func TestUpsertUnderwriting(t *testing.T) {
 		AverageTransactionSize:          1_500,
 		MaxTransactionSize:              1_200,
 		AverageMonthlyTransactionVolume: 11_000,
+		Fulfillment: moov.Fulfillment{
+			HasPhysicalGoods:     false,
+			IsShippingProduct:    false,
+			ShipmentDurationDays: 0,
+			ReturnPolicy:         moov.NONE,
+		},
+		CardVolumeDistribution: moov.CardVolumeDistribution{
+			EcommercePercentage:     40,
+			CardPresentPercentage:   40,
+			MailOrPhonePercentage:   10,
+			DebtRepaymentPercentage: 10,
+		},
+		VolumeByCustomerType: moov.VolumeByCustomerType{
+			BusinessToBusinessPercentage: 70,
+			ConsumerToBusinessPercentage: 30,
+		},
 	}
 
 	t.Run("update", func(t *testing.T) {
 		actual, err := mc.UpsertUnderwriting(context.Background(), account.AccountID, update)
 
 		NoResponseError(t, err)
+		require.NotNil(t, actual)
 		require.Equal(t, update.AverageTransactionSize, actual.AverageTransactionSize)
 		require.Equal(t, update.MaxTransactionSize, actual.MaxTransactionSize)
 		require.Equal(t, update.AverageMonthlyTransactionVolume, actual.AverageMonthlyTransactionVolume)
+		//todo: comment in once the flag is enabled
+		// require.Equal(t, update.Fulfillment, actual.Fulfillment)
+		// require.Equal(t, update.CardVolumeDistribution, actual.CardVolumeDistribution)
+		// require.Equal(t, update.VolumeByCustomerType, actual.VolumeByCustomerType)
 		require.Equal(t, moov.UnderwritingStatusNotRequested, actual.Status)
 	})
 
@@ -59,9 +106,14 @@ func TestUpsertUnderwriting(t *testing.T) {
 		actual, err := mc.GetUnderwriting(context.Background(), account.AccountID)
 
 		NoResponseError(t, err)
+		require.NotNil(t, actual)
 		require.Equal(t, update.AverageTransactionSize, actual.AverageTransactionSize)
 		require.Equal(t, update.MaxTransactionSize, actual.MaxTransactionSize)
 		require.Equal(t, update.AverageMonthlyTransactionVolume, actual.AverageMonthlyTransactionVolume)
+		//todo: comment in once the flag is enabled
+		// require.Equal(t, update.Fulfillment, actual.Fulfillment)
+		// require.Equal(t, update.CardVolumeDistribution, actual.CardVolumeDistribution)
+		// require.Equal(t, update.VolumeByCustomerType, actual.VolumeByCustomerType)
 		require.Equal(t, moov.UnderwritingStatusNotRequested, actual.Status)
 	})
 }
--- pkg/moov/undewriting_models.go
+++ pkg/moov/undewriting_models.go
@@ -1,16 +1,22 @@
 package moov
 
 type UpdateUnderwriting struct {
-	AverageTransactionSize          int64 `json:"averageTransactionSize"`
-	MaxTransactionSize              int64 `json:"maxTransactionSize"`
-	AverageMonthlyTransactionVolume int64 `json:"averageMonthlyTransactionVolume"`
+	AverageTransactionSize          int64                  `json:"averageTransactionSize"`
+	MaxTransactionSize              int64                  `json:"maxTransactionSize"`
+	AverageMonthlyTransactionVolume int64                  `json:"averageMonthlyTransactionVolume"`
+	VolumeByCustomerType            VolumeByCustomerType   `json:"volumeByCustomerType"`
+	CardVolumeDistribution          CardVolumeDistribution `json:"cardVolumeDistribution"`
+	Fulfillment                     Fulfillment            `json:"fulfillment"`
 }
 
 type Underwriting struct {
-	AverageTransactionSize          int64              `json:"averageTransactionSize"`
-	MaxTransactionSize              int64              `json:"maxTransactionSize"`
-	AverageMonthlyTransactionVolume int64              `json:"averageMonthlyTransactionVolume"`
-	Status                          UnderwritingStatus `json:"status"`
+	AverageTransactionSize          int64                  `json:"averageTransactionSize"`
+	MaxTransactionSize              int64                  `json:"maxTransactionSize"`
+	AverageMonthlyTransactionVolume int64                  `json:"averageMonthlyTransactionVolume"`
+	Status                          UnderwritingStatus     `json:"status"`
+	VolumeByCustomerType            VolumeByCustomerType   `json:"volumeByCustomerType"`
+	CardVolumeDistribution          CardVolumeDistribution `json:"cardVolumeDistribution"`
+	Fulfillment                     Fulfillment            `json:"fulfillment"`
 }
 
 type UnderwritingStatus string
@@ -22,3 +28,31 @@ const (
 	UnderwritingStatusPending       UnderwritingStatus = "pending"
 	UnderwritingStatusNotRequested  UnderwritingStatus = "notRequested"
 )
+
+type VolumeByCustomerType struct {
+	BusinessToBusinessPercentage int32 `json:"businessToBusinessPercentage"`
+	ConsumerToBusinessPercentage int32 `json:"consumerToBusinessPercentage"`
+}
+
+type CardVolumeDistribution struct {
+	EcommercePercentage     int32 `json:"ecommercePercentage"`
+	CardPresentPercentage   int32 `json:"cardPresentPercentagePercentage"`
+	MailOrPhonePercentage   int32 `json:"mailOrPhonePercentage"`
+	DebtRepaymentPercentage int32 `json:"debtRepaymentPercentage"`
+}
+
+type ReturnPolicy string
+
+const (
+	NONE               ReturnPolicy = "none"
+	EXCHANGE_ONLY      ReturnPolicy = "exchangeOnly"
+	WITHIN_THIRTY_DAYS ReturnPolicy = "withinThirtyDays"
+	OTHER              ReturnPolicy = "other"
+)
+
+type Fulfillment struct {
+	HasPhysicalGoods     bool         `json:"hasPhysicalGoods"`
+	IsShippingProduct    bool         `json:"isShippingProduct"`
+	ShipmentDurationDays int32        `json:"shipmentDurationDays"`
+	ReturnPolicy         ReturnPolicy `json:"returnPolicy"`
+}
