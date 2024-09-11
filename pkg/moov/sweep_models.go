a2777354a5c3c558ae1cfb121516dea36a2cb1b9
Vincent Xiao <vincentx72@gmail.com>
2024-09-11 14:42:18 -0700

    sweeps: add LIST and GET sweep endpoints

--- pkg/moov/sweep_models.go
+++ pkg/moov/sweep_models.go
@@ -32,3 +32,36 @@ type SweepConfigPaymentMethod struct {
 	PaymentMethodID string     `json:"paymentMethodID"`
 	DisabledOn      *time.Time `json:"disabledOn,omitempty"`
 }
+
+type Sweep struct {
+	SweepID             string      `json:"sweepID,omitempty"`
+	Status              SweepStatus `json:"status,omitempty"`
+	AccrualStartedOn    time.Time   `json:"accrualStartedOn,omitempty"`
+	AccrualEndedOn      *time.Time  `json:"accrualEndedOn,omitempty"`
+	PushPaymentMethodID string      `json:"pushPaymentMethodID,omitempty"`
+	PullPaymentMethodID string      `json:"pullPaymentMethodID,omitempty"`
+	// The total net Amount of wallet transactions accrued in the sweep.
+	// A decimal-formatted numerical string that represents up to 9 decimal place precision.
+	AccruedAmount string `json:"accruedAmount,omitempty"`
+	// A 3-letter ISO 4217 currency code.
+	Currency string `json:"currency,omitempty"`
+	// Only set after the sweep transfer has been created.
+	TransferID string `json:"transferID,omitempty"`
+	// Only set after the sweep transfer has been created.
+	// A decimal-formatted numerical string that represents up to 9 decimal place precision.
+	TransferAmount string `json:"transferAmount,omitempty"`
+	// Amount remaining in the wallet after the sweep transfer completes.
+	// A decimal-formatted numerical string that represents up to 9 decimal place precision.
+	ResidualBalance string `json:"residualBalance,omitempty"`
+}
+
+type SweepStatus string
+
+const (
+	SweepStatus_Accruing       SweepStatus = "accruing"
+	SweepStatus_ActionRequired SweepStatus = "action-required"
+	SweepStatus_Canceled       SweepStatus = "canceled"
+	SweepStatus_Closed         SweepStatus = "closed"
+	SweepStatus_Failed         SweepStatus = "failed"
+	SweepStatus_Paid           SweepStatus = "paid"
+)
