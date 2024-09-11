0a0543364e2b905b398c26ad120903acbe1530bf
Vincent Xiao <vincentx72@gmail.com>
2024-09-11 10:10:29 -0700

    update IDs for prod sandbox test env

--- pkg/moov/sweep_test.go
+++ pkg/moov/sweep_test.go
@@ -9,15 +9,15 @@ import (
 	"github.com/moovfinancial/moov-go/pkg/moov"
 )
 
-// TODO(vince): update IDs here once I get invited to the prod sandbox account
 func Test_SweepConfig_CRUD_Endpoints(t *testing.T) {
 	var (
 		mc = NewTestClient(t)
 
-		ctx        = context.Background()
-		accountID  = "ff54f56f-6b7e-439f-9280-bc5edd3f93fc"
-		walletID   = "24ff6ab1-01c0-4c50-bd61-d9fe096a0b72"
-		minBalance = "100.0"
+		ctx = context.Background()
+		// IDs for Lincoln National Corporation
+		accountID  = "ebbf46c6-122a-4367-bc45-7dd555e1d3b9"
+		walletID   = "4dbac313-d505-4d51-a0fe-c11787916fcf"
+		minBalance = "1000.0"
 	)
 
 	sweepConfigs, err := mc.ListSweepConfigs(ctx, accountID)
@@ -27,12 +27,11 @@ func Test_SweepConfig_CRUD_Endpoints(t *testing.T) {
 	// If no sweep configs found, create one
 	if len(sweepConfigs) == 0 {
 		sweepConfig, err = mc.CreateSweepConfig(ctx, moov.CreateSweepConfig{
-			AccountID: accountID,
-			WalletID:  walletID,
-			Status:    moov.SweepConfigStatus_Enabled,
-			// todo: update these
-			PushPaymentMethodID: "df0ac7e9-639b-41ca-ab0e-7180f356ed86",
-			PullPaymentMethodID: "155647dd-38ae-4ac6-a9a4-8c02bec9141e",
+			AccountID:           accountID,
+			WalletID:            walletID,
+			Status:              moov.SweepConfigStatus_Enabled,
+			PushPaymentMethodID: "b46193d2-6b9b-4a73-afdc-3871779f51e3",
+			PullPaymentMethodID: "467dfcdc-463b-4282-83af-db6f47562bf9",
 			MinimumBalance:      &minBalance,
 		})
 
