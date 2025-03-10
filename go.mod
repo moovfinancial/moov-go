79d48396671facae1934a3b552d2ecd35ec87a58
Vincent Xiao <vincentx72@gmail.com>
2025-03-10 15:42:26 -0700

    cmt test

--- go.mod
+++ go.mod
@@ -11,9 +11,41 @@ require (
 )
 
 require (
+	github.com/antihax/optional v1.0.0 // indirect
+	github.com/cenkalti/backoff/v4 v4.3.0 // indirect
 	github.com/davecgh/go-spew v1.1.2-0.20180830191138-d8f796af33cc // indirect
+	github.com/go-kit/log v0.2.1 // indirect
+	github.com/go-logfmt/logfmt v0.6.0 // indirect
+	github.com/go-logr/logr v1.4.2 // indirect
+	github.com/go-logr/stdr v1.2.2 // indirect
+	github.com/go-ozzo/ozzo-validation v3.6.0+incompatible // indirect
+	github.com/go-ozzo/ozzo-validation/v4 v4.3.0 // indirect
+	github.com/grpc-ecosystem/grpc-gateway/v2 v2.22.0 // indirect
+	github.com/moov-io/base v0.52.1 // indirect
+	github.com/moov-io/iso4217 v0.3.2 // indirect
+	github.com/moovfinancial/errors v1.4.6 // indirect
+	github.com/moovfinancial/events v0.189.4 // indirect
+	github.com/moovfinancial/go-observability/v2 v2.35.1 // indirect
+	github.com/moovfinancial/money v0.22.0 // indirect
+	github.com/moovfinancial/platform-api v0.0.0-20241212194244-0f8c0666395a // indirect
 	github.com/pmezard/go-difflib v1.0.1-0.20181226105442-5d4384ee4fb2 // indirect
+	github.com/shopspring/decimal v1.4.0 // indirect
+	go.opentelemetry.io/otel v1.30.0 // indirect
+	go.opentelemetry.io/otel/exporters/otlp/otlptrace v1.30.0 // indirect
+	go.opentelemetry.io/otel/exporters/otlp/otlptrace/otlptracegrpc v1.30.0 // indirect
+	go.opentelemetry.io/otel/exporters/stdout/stdouttrace v1.30.0 // indirect
+	go.opentelemetry.io/otel/metric v1.30.0 // indirect
+	go.opentelemetry.io/otel/sdk v1.30.0 // indirect
+	go.opentelemetry.io/otel/trace v1.30.0 // indirect
+	go.opentelemetry.io/proto/otlp v1.3.1 // indirect
 	golang.org/x/crypto v0.32.0 // indirect
+	golang.org/x/net v0.29.0 // indirect
+	golang.org/x/oauth2 v0.22.0 // indirect
+	golang.org/x/sys v0.29.0 // indirect
 	golang.org/x/text v0.21.0 // indirect
+	google.golang.org/genproto/googleapis/api v0.0.0-20240903143218-8af14fe29dc1 // indirect
+	google.golang.org/genproto/googleapis/rpc v0.0.0-20240903143218-8af14fe29dc1 // indirect
+	google.golang.org/grpc v1.67.0 // indirect
+	google.golang.org/protobuf v1.34.2 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
--- pkg/moov/demo_test.go
+++ pkg/moov/demo_test.go
@@ -9,41 +9,82 @@ import (
 	"strings"
 	"testing"
 
+	"github.com/moovfinancial/money/v2"
 	"github.com/moovfinancial/moov-go/pkg/moov"
 	"github.com/stretchr/testify/require"
 )
+
+/*
+Problem:
+Cancelling a transfer through the Moov Dashboard is great
+if you just have to cancel one or a few transfers.
+
+But what if you have A LOT of transfers to cancel?
+It's not practical to cancel them one-by-one in the Dashboard.
+
+Also, cancellation windows may be shorter due to settlement times.
+
+This can be harder if transfers that should be canceled
+are sprinkled between transfers that should NOT be canceled.
+
+
+Solution:
+Use our API -- or even better, use our SDK where we've alredy done all the plumbing for you
+-- to cancel transfers in bulk.
+
+
+To determine which transfers to cancel,
+rather than validating manually in the Dashbaord -- you can use properties of the transfer
+(or any other object in our API) to validate whether to cancel a certain transfer or not.
+
+*/
 
 const (
 	partnerAccountID       = "db04bf9d-91f6-4206-ba38-6844636532ad"
 	awesomeDronesAccountID = "e5a43c02-559a-49e4-a0f8-5b180ec30202"
 	dbFilename             = "sqlite.txt"
+
+	maxXfrAmt = 20
+
+	// total transfers created = 3  * numTransfersPerType
+	numTransfersPerType = 20
 )
 
-func TestX(t *testing.T) {
+/*
+Simulates a scenario where lots of transfers are created, but only some need to be canceled.
+
+Create 60 transfers:
+- 20 card-to-wallet
+- 20 bank-to-wallet
+- 20 bank-to-bank
+
+Each transfer has a random amount between $1 and $20.
+
+🐛 I have a bug in here where:
+there's a 33% chance the transfer amount will be multipled by 100.
+e.g. so instead of a $14 transfer, it'll be $1400.
+*/
+func TestDemo_CreateTransfers(t *testing.T) {
 	mc := NewTestClient(t)
 	require := require.New(t)
 	ctx := context.Background()
 
-	max := 20
-
-	options, err := mc.TransferOptions(BgCtx(), moov.CreateTransferOptions{
+	// Generating transfer options for source=AwesomeDrones and destination=MoovFinancial (partner)
+	options, err := mc.TransferOptions(ctx, moov.CreateTransferOptions{
 		Source:      moov.CreateTransferOptionsTarget{AccountID: awesomeDronesAccountID},
 		Destination: moov.CreateTransferOptionsTarget{AccountID: partnerAccountID},
-		Amount:      moov.Amount{Currency: "USD", Value: int64(100 * max)},
+		Amount:      moov.Amount{Currency: "USD", Value: int64(100 * maxXfrAmt)},
 	})
 	NoResponseError(t, err)
 
 	var (
-		// pmIds
 		cardPMID         string
 		achDebitFundPMID string
-
-		//
 		walletPMID       string
 		achCreditStdPMID string
 	)
-	fmt.Println(achDebitFundPMID, achCreditStdPMID)
 
+	// Collecting payment method IDs from the transfer options
 	for _, opt := range options.SourceOptions {
 		if opt.PaymentMethodType == moov.PaymentMethodType_CardPayment {
 			cardPMID = opt.PaymentMethodID
@@ -60,86 +101,133 @@ func TestX(t *testing.T) {
 		}
 	}
 
-	var createdXfrIDs []string
-	create := func(srcPMID, dstPMID string) {
-		randomNumber := rand.Intn(max) + 1
-		randPercentage := rand.Float32() * .33
+	var (
+		createdXfrIDs []string
+		numBadXfrs    int
+	)
+	// A function to create a transfer
+	create := func(srcPMID, dstPMID string, xfrType string) {
+		randomNumber := rand.Intn(maxXfrAmt) + 1
 
 		amt := moov.Amount{
 			Currency: "USD",
 			Value:    int64(100 * randomNumber),
 		}
-		if randPercentage <= .33 {
+		hasBug := false
+		randPercentage := rand.Float32()
+
+		// 🐛🐛🐛🐛🐛🐛🐛🐛🐛🐛🐛
+		if randPercentage <= .33333 {
+			hasBug = true
 			amt.Value *= 100
+			numBadXfrs++
 		}
 
-		completed, _, err := mc.CreateTransfer(BgCtx(), partnerAccountID, moov.CreateTransfer{
+		xfr, _, err := mc.CreateTransfer(ctx, partnerAccountID, moov.CreateTransfer{
 			Source:      moov.CreateTransfer_Source{PaymentMethodID: srcPMID},
 			Destination: moov.CreateTransfer_Destination{PaymentMethodID: dstPMID},
 			Amount:      amt,
 		}).WaitForRailResponse()
 		NoResponseError(t, err)
 
-		createdXfrIDs = append(createdXfrIDs, completed.TransferID)
+		createdXfrIDs = append(createdXfrIDs, xfr.TransferID)
+
+		if hasBug {
+			fmt.Printf("😨😨😨 Created %s %s transfer\n", prettyPrintAmount(xfr.Amount), xfrType)
+		} else {
+			fmt.Printf("💸 Created %s transfer\n", prettyPrintAmount(xfr.Amount))
+		}
 
 	}
 
+	// Collect all transferIDs and writing the IDs to sqlite.txt file
 	f, err := os.Create(dbFilename)
 	require.NoError(err)
 	defer f.Close()
+
+	fmt.Printf("🤑 Creating transfers 🤑\n\n")
+
+	for range numTransfersPerType {
+		create(cardPMID, walletPMID, "card-to-wallet")
+	}
+
+	for range numTransfersPerType {
+		create(achDebitFundPMID, walletPMID, "bank-to-wallet")
+	}
+
+	for range numTransfersPerType {
+		create(achDebitFundPMID, achCreditStdPMID, "bank-to-bank")
+	}
 
 	b := strings.Join(createdXfrIDs, "\n")
 	_, err = f.WriteString(b)
 	require.NoError(err)
 
-	numTransfersPerType := 1
+	// Print results
+	fmt.Println()
+	fmt.Println("Result: ")
+	fmt.Printf("Created %d transfers successfully\n", len(createdXfrIDs))
+	fmt.Printf("But %d out of %d transfers have wrong amounts \n\n", numBadXfrs, len(createdXfrIDs))
+}
 
-	for range numTransfersPerType {
-		create(cardPMID, walletPMID) // card-to-wallet
-	}
+/*
+Cancels the transfers with invalid amounts:
+1. Will fetch the transferIDs from sqlite.txt file
+2. Determine if the transfer should be canceled (by checking if amount > $100)
+3. Initiate the cancellation
+4. Check the the cancellation completed
+*/
+func TestDemo_CancelTransfers(t *testing.T) {
+	mc := NewTestClient(t)
+	require := require.New(t)
+	ctx := context.Background()
 
-	for range numTransfersPerType {
-		create(achDebitFundPMID, walletPMID) // bank-to-wallet
-	}
-
-	for range numTransfersPerType {
-		create(achDebitFundPMID, achCreditStdPMID) // bank-to-bank
-	}
-
-	f, err = os.Open(dbFilename)
+	f, err := os.Open(dbFilename)
 	require.NoError(err)
 	defer f.Close()
 
 	scanner := bufio.NewScanner(f)
 
-	var xfrIDs []string
+	xfrIDsToAmount := map[string]moov.Amount{}
 	for scanner.Scan() {
 		xfrID := scanner.Text()
 		if len(xfrID) == 0 {
 			continue
 		}
 
-		xfr, err := mc.GetTransfer(ctx, partnerapartnerAccountID, xfrID)
-		require.NoError(err, )
+		xfr, err := mc.GetTransfer(ctx, partnerAccountID, xfrID)
+		require.NoError(err)
 
-
-		createdXfrIDs = append(createdXfrIDs, xfrID)
+		// Cancel any transfers greater than $100
+		if xfr.Amount.Value > 100_00 {
+			xfrIDsToAmount[xfr.TransferID] = xfr.Amount
+		}
 	}
 
-	for _, xfrID := range createdXfrIDs {
-		cancellation, err := mc.CancelTransfer(BgCtx(), partnerAccountID, xfrID)
-		NoResponseError(t, err)
+	var numCancellations int
 
-		require.Equal(moov.CancellationStatus_Completed, cancellation.Status)
+	fmt.Println("⚠️ Cancelling transfers ⚠️")
+	fmt.Println()
+	for xfrID, amount := range xfrIDsToAmount {
+		cancellation, err := mc.CancelTransfer(ctx, partnerAccountID, xfrID)
+		if err != nil {
+			fmt.Println("Error: ", err)
+		} else {
+			require.Equal(moov.CancellationStatus_Completed, cancellation.Status)
+			fmt.Printf("❌ Canceled transfer with amount %s\n", prettyPrintAmount(amount))
+
+			numCancellations += 1
+		}
 	}
 
-	// t.Run("create and get cancellation", func(t *testing.T) {
-	// 	createdCancellation, err := mc.CancelTransfer(BgCtx(), FACILITATOR_ID, transferID)
-	// 	NoResponseError(t, err)
-	// 	require.NotEmpty(t, createdCancellation)
+	fmt.Printf("\n🎉 Canceled %d transfers\n\n\n", numCancellations)
+}
 
-	// 	fetchedCancellation, err := mc.GetCancellation(BgCtx(), FACILITATOR_ID, transferID, createdCancellation.CancellationID)
-	// 	NoResponseError(t, err)
-	// 	require.Equal(t, createdCancellation.CancellationID, fetchedCancellation.CancellationID)
-	// })
+func prettyPrintAmount(amt moov.Amount) string {
+	moneyAmt, err := money.FromInt64(amt.Value, amt.Currency)
+	if err != nil {
+		panic(err)
+	}
+
+	return "$" + moneyAmt.ValueString()
 }
