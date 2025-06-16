3ee3d33705edce0020f08d780404bcd9a11679b3
droachmoov <david@moov.io>
2025-06-16 07:39:17 -0700

    [COR-3748] moved logic

--- pkg/moov/underwriting_api.go
+++ pkg/moov/underwriting_api.go
@@ -5,6 +5,33 @@ import (
 	"net/http"
 )
 
+// UpsertUnderwriting adds or updates underwriting information for the given account.
+// The account must have a description and an MCC set to create underwriting.
+// Returns the underwriting information for the account.
+func (c Client) UpsertUnderwriting(ctx context.Context, accountID string, underwriting UpdateUnderwriting) (*Underwriting, error) {
+	resp, err := c.CallHttp(ctx,
+		Endpoint(http.MethodPut, pathUnderwriting, accountID),
+		AcceptJson(),
+		JsonBody(underwriting))
+	if err != nil {
+		return nil, err
+	}
+
+	return CompletedObjectOrError[Underwriting](resp)
+}
+
+// GetUnderwriting returns the underwriting information for the given account.
+func (c Client) GetUnderwriting(ctx context.Context, accountID string) (*Underwriting, error) {
+	resp, err := c.CallHttp(ctx,
+		Endpoint(http.MethodGet, pathUnderwriting, accountID),
+		AcceptJson())
+	if err != nil {
+		return nil, err
+	}
+
+	return CompletedObjectOrError[Underwriting](resp)
+}
+
 type UnderwritingClient[T any, V any] struct {
 	Version string
 }
@@ -32,31 +59,4 @@ func (uc UnderwritingClient[T, V]) Upsert(ctx context.Context, client Client, ac
 	}
 
 	return CompletedObjectOrError[V](httpResp)
-}
-
-// UpsertUnderwriting adds or updates underwriting information for the given account.
-// The account must have a description and an MCC set to create underwriting.
-// Returns the underwriting information for the account.
-func (c Client) UpsertUnderwriting(ctx context.Context, accountID string, underwriting UpdateUnderwriting) (*Underwriting, error) {
-	resp, err := c.CallHttp(ctx,
-		Endpoint(http.MethodPut, pathUnderwriting, accountID),
-		AcceptJson(),
-		JsonBody(underwriting))
-	if err != nil {
-		return nil, err
-	}
-
-	return CompletedObjectOrError[Underwriting](resp)
-}
-
-// GetUnderwriting returns the underwriting information for the given account.
-func (c Client) GetUnderwriting(ctx context.Context, accountID string) (*Underwriting, error) {
-	resp, err := c.CallHttp(ctx,
-		Endpoint(http.MethodGet, pathUnderwriting, accountID),
-		AcceptJson())
-	if err != nil {
-		return nil, err
-	}
-
-	return CompletedObjectOrError[Underwriting](resp)
 }
