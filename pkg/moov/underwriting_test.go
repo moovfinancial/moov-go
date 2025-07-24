e17817813fe312bf9174aa6bc492f58a48b24573
droachmoov <david@moov.io>
2025-07-24 13:01:44 -0700

    [COR-3877] Add submission intent to underwriting POST

--- pkg/moov/underwriting_test.go
+++ pkg/moov/underwriting_test.go
@@ -216,3 +216,107 @@ func TestUpsertUnderwriting_V2507(t *testing.T) {
 		require.Equal(t, update.MoneyTransfer, actual.MoneyTransfer)
 	})
 }
+
+func TestUpsertUnderwriting_V2507(t *testing.T) {
+	mc := NewTestClient(t)
+
+	account := CreateTemporaryTestAccount(t, mc, createTestBusinessAccount())
+
+	create := mv2507.UpsertUnderwriting{
+		GeographicReach: moov.PtrOf(mv2507.GeographicReachUsAndInternational),
+		CollectFunds: &mv2507.CollectFunds{
+			CardPayments: &mv2507.CollectFundsCardPayments{
+				EstimatedActivity: &mv2507.EstimatedActivity{
+					MonthlyVolumeRange: moov.PtrOf(mv2507.MonthlyVolumeRangeUnder10K),
+				},
+			},
+		},
+		SubmissionIntent: moov.PtrOf(mv2507.SubmissionIntentWait),
+	}
+
+	t.Run("insert", func(t *testing.T) {
+		actual, err := mv2507.Underwriting.Upsert(context.Background(), *mc, account.AccountID, create)
+
+		NoResponseError(t, err)
+		require.NotNil(t, actual)
+		require.Equal(t, create.GeographicReach, actual.GeographicReach)
+		require.Equal(t, create.CollectFunds, actual.CollectFunds)
+		require.Nil(t, actual.BusinessPresence)
+		require.Nil(t, actual.PendingLitigation)
+		require.Nil(t, actual.VolumeShareByCustomerType)
+		require.Nil(t, actual.SendFunds)
+		require.Nil(t, actual.MoneyTransfer)
+	})
+
+	t.Run("get", func(t *testing.T) {
+		actual, err := mv2507.Underwriting.Get(context.Background(), *mc, account.AccountID)
+
+		NoResponseError(t, err)
+		require.NotNil(t, actual)
+		require.Equal(t, create.GeographicReach, actual.GeographicReach)
+		require.Equal(t, create.CollectFunds, actual.CollectFunds)
+		require.Nil(t, actual.BusinessPresence)
+		require.Nil(t, actual.PendingLitigation)
+		require.Nil(t, actual.VolumeShareByCustomerType)
+		require.Nil(t, actual.SendFunds)
+		require.Nil(t, actual.MoneyTransfer)
+	})
+
+	update := mv2507.UpsertUnderwriting{
+		GeographicReach:   moov.PtrOf(mv2507.GeographicReachUsOnly),
+		BusinessPresence:  moov.PtrOf(mv2507.BusinessPresenceHomeBased),
+		PendingLitigation: moov.PtrOf(mv2507.PendingLitigationNone),
+		VolumeShareByCustomerType: &mv2507.VolumeShareByCustomerType{
+			Business: moov.PtrOf(70),
+			Consumer: moov.PtrOf(30),
+			P2P:      moov.PtrOf(0),
+		},
+		CollectFunds: &mv2507.CollectFunds{
+			CardPayments: &mv2507.CollectFundsCardPayments{
+				EstimatedActivity: &mv2507.EstimatedActivity{MonthlyVolumeRange: moov.PtrOf(mv2507.MonthlyVolumeRange10K50K)},
+			},
+			Ach: &mv2507.CollectFundsAch{
+				EstimatedActivity: &mv2507.EstimatedActivity{MonthlyVolumeRange: moov.PtrOf(mv2507.MonthlyVolumeRangeUnder10K)},
+			},
+		},
+		SendFunds: &mv2507.SendFunds{
+			Ach: &mv2507.SendFundsAch{
+				EstimatedActivity: &mv2507.EstimatedActivity{MonthlyVolumeRange: moov.PtrOf(mv2507.MonthlyVolumeRangeUnder10K)},
+			},
+		},
+		MoneyTransfer: &mv2507.MoneyTransfer{
+			PullFromCard: &mv2507.MoneyTransferPullFromCard{
+				EstimatedActivity: &mv2507.EstimatedActivity{MonthlyVolumeRange: moov.PtrOf(mv2507.MonthlyVolumeRange10K50K)},
+			},
+		},
+		SubmissionIntent: moov.PtrOf(mv2507.SubmissionIntentSubmit),
+	}
+
+	t.Run("update", func(t *testing.T) {
+		actual, err := mv2507.Underwriting.Upsert(context.Background(), *mc, account.AccountID, update)
+
+		NoResponseError(t, err)
+		require.NotNil(t, actual)
+		require.Equal(t, update.GeographicReach, actual.GeographicReach)
+		require.Equal(t, update.BusinessPresence, actual.BusinessPresence)
+		require.Equal(t, update.PendingLitigation, actual.PendingLitigation)
+		require.Equal(t, update.VolumeShareByCustomerType, actual.VolumeShareByCustomerType)
+		require.Equal(t, update.CollectFunds, actual.CollectFunds)
+		require.Equal(t, update.SendFunds, actual.SendFunds)
+		require.Equal(t, update.MoneyTransfer, actual.MoneyTransfer)
+	})
+
+	t.Run("get after update", func(t *testing.T) {
+		actual, err := mv2507.Underwriting.Get(context.Background(), *mc, account.AccountID)
+
+		NoResponseError(t, err)
+		require.NotNil(t, actual)
+		require.Equal(t, update.GeographicReach, actual.GeographicReach)
+		require.Equal(t, update.BusinessPresence, actual.BusinessPresence)
+		require.Equal(t, update.PendingLitigation, actual.PendingLitigation)
+		require.Equal(t, update.VolumeShareByCustomerType, actual.VolumeShareByCustomerType)
+		require.Equal(t, update.CollectFunds, actual.CollectFunds)
+		require.Equal(t, update.SendFunds, actual.SendFunds)
+		require.Equal(t, update.MoneyTransfer, actual.MoneyTransfer)
+	})
+}
