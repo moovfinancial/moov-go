676a84fa6f9e70a6c5c9884d51aebb2888d7cc60
Vincent Xiao <vincentx72@gmail.com>
2025-12-15 15:08:14 -0800

    invoices: add initial models and endpoints

---
+++ pkg/moov/invoice_api.go
@@ -0,0 +1,124 @@
+package moov
+
+import (
+	"context"
+	"fmt"
+	"net/http"
+)
+
+// CreateInvoice creates a new invoice for a Moov account
+func (c Client) CreateInvoice(ctx context.Context, accountID string, invoice CreateInvoice) (*Invoice, error) {
+	resp, err := c.CallHttp(ctx,
+		Endpoint(http.MethodPost, pathInvoices, accountID),
+		AcceptJson(),
+		JsonBody(invoice),
+	)
+	if err != nil {
+		return nil, err
+	}
+
+	out, err := UnmarshalObjectResponse[Invoice](resp)
+	return out, err
+}
+
+// GetInvoice retrieves an invoice by ID
+func (c Client) GetInvoice(ctx context.Context, accountID, invoiceID string) (*Invoice, error) {
+	resp, err := c.CallHttp(ctx,
+		Endpoint(http.MethodGet, pathInvoice, accountID, invoiceID),
+		AcceptJson(),
+	)
+	if err != nil {
+		return nil, err
+	}
+
+	return CompletedObjectOrError[Invoice](resp)
+}
+
+// UpdateInvoice updates an existing invoice
+func (c Client) UpdateInvoice(ctx context.Context, accountID, invoiceID string, invoice UpdateInvoice) (*Invoice, error) {
+	resp, err := c.CallHttp(ctx,
+		Endpoint(http.MethodPatch, pathInvoice, accountID, invoiceID),
+		AcceptJson(),
+		JsonBody(invoice),
+	)
+	if err != nil {
+		return nil, err
+	}
+
+	return CompletedObjectOrError[Invoice](resp)
+}
+
+// MarkInvoicePaid marks an invoice as paid outside of the Moov platform
+func (c Client) MarkInvoicePaid(ctx context.Context, accountID, invoiceID string, payment MarkInvoicePaid) (*Invoice, error) {
+	resp, err := c.CallHttp(ctx,
+		Endpoint(http.MethodPut, pathInvoiceMarkPaid, accountID, invoiceID),
+		AcceptJson(),
+		JsonBody(payment),
+	)
+	if err != nil {
+		return nil, err
+	}
+
+	return CompletedObjectOrError[Invoice](resp)
+}
+
+// TODO(vince,12/15/2025): Uncomment once this is fully supported in production.
+// SendInvoice finalizes an invoice, creates and sends a payment link
+// func (c Client) SendInvoice(ctx context.Context, accountID, invoiceID string) (*Invoice, error) {
+// 	resp, err := c.CallHttp(ctx,
+// 		Endpoint(http.MethodPost, pathInvoiceSend, accountID, invoiceID),
+// 		AcceptJson(),
+// 	)
+// 	if err != nil {
+// 		return nil, err
+// 	}
+
+// 	return CompletedObjectOrError[Invoice](resp)
+// }
+
+// ListInvoiceFilter represents a filter option for listing invoices
+type ListInvoiceFilter callArg
+
+// WithInvoiceStatus filters invoices by status
+func WithInvoiceStatus(status InvoiceStatus) ListInvoiceFilter {
+	return callBuilderFn(func(call *callBuilder) error {
+		call.params["status"] = string(status)
+		return nil
+	})
+}
+
+// WithInvoiceCustomerAccountID filters invoices by customer account ID
+func WithInvoiceCustomerAccountID(customerAccountID string) ListInvoiceFilter {
+	return callBuilderFn(func(call *callBuilder) error {
+		call.params["customerAccountID"] = customerAccountID
+		return nil
+	})
+}
+
+// WithInvoiceCount sets the number of invoices to return
+func WithInvoiceCount(count int) ListInvoiceFilter {
+	return callBuilderFn(func(call *callBuilder) error {
+		call.params["count"] = fmt.Sprintf("%d", count)
+		return nil
+	})
+}
+
+// WithInvoiceSkip sets the number of invoices to skip
+func WithInvoiceSkip(skip int) ListInvoiceFilter {
+	return callBuilderFn(func(call *callBuilder) error {
+		call.params["skip"] = fmt.Sprintf("%d", skip)
+		return nil
+	})
+}
+
+// ListInvoices lists all invoices for a Moov account
+// https://docs.moov.io/api/
+func (c Client) ListInvoices(ctx context.Context, accountID string, filters ...ListInvoiceFilter) ([]Invoice, error) {
+	args := prependArgs(filters, AcceptJson())
+	resp, err := c.CallHttp(ctx, Endpoint(http.MethodGet, pathInvoices, accountID), args...)
+	if err != nil {
+		return nil, err
+	}
+
+	return CompletedListOrError[Invoice](resp)
+}
---
+++ pkg/moov/invoice_test.go
@@ -0,0 +1,71 @@
+package moov_test
+
+import (
+	"testing"
+	"time"
+
+	"github.com/stretchr/testify/require"
+
+	"github.com/moovfinancial/moov-go/internal/testtools"
+	"github.com/moovfinancial/moov-go/pkg/moov"
+)
+
+func Test_Invoice_CreateUpdateGet(t *testing.T) {
+	var (
+		mc                = NewTestClient(t)
+		ctx               = t.Context()
+		accountID         = testtools.MERCHANT_ID
+		customerAccountID = testtools.PARTNER_ID
+	)
+
+	create := moov.CreateInvoice{
+		CustomerAccountID: customerAccountID,
+		Description:       "Testing moov-go",
+		LineItems: moov.CreateInvoiceLineItems{
+			Items: []moov.CreateInvoiceLineItem{
+				{
+					Name: "Payment for developing this SDK",
+					BasePrice: moov.AmountDecimal{
+						Currency:     "USD",
+						ValueDecimal: "1.00",
+					},
+					Quantity: 1,
+					Options: []moov.CreateInvoiceLineItemOption{
+						moov.CreateInvoiceLineItemOption{
+							Name:     "big TIP",
+							Quantity: 1,
+							PriceModifier: &moov.AmountDecimal{
+								Currency:     "USD",
+								ValueDecimal: "3.50",
+							},
+						},
+					},
+				},
+			},
+		},
+	}
+
+	// create the invoice
+	createdInvoice, err := mc.CreateInvoice(ctx, accountID, create)
+	require.NoError(t, err)
+
+	// fetch by ID
+	fetchedInvoice, err := mc.GetInvoice(ctx, accountID, createdInvoice.InvoiceID)
+	require.NoError(t, err)
+	require.Equal(t, createdInvoice, fetchedInvoice)
+
+	// list by status
+	listedInvoices, err := mc.ListInvoices(ctx, accountID,
+		moov.WithInvoiceStatus(moov.InvoiceStatusDraft),
+		moov.WithInvoiceCustomerAccountID(customerAccountID))
+	require.NoError(t, err)
+	latestInvoice := listedInvoices[0]
+	require.Equal(t, *createdInvoice, latestInvoice)
+
+	// Update the dueDate
+	now := time.Now().UTC()
+	update := moov.UpdateInvoice{DueDate: moov.PtrOf(now)}
+	updatedInvoice, err := mc.UpdateInvoice(ctx, accountID, createdInvoice.InvoiceID, update)
+	require.NoError(t, err)
+	require.Equal(t, now, *updatedInvoice.DueDate)
+}
