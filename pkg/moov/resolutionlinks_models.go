31c7ab3bc895e03a16520ced5063bfce9c09e9a4
Blake Brauer <blake.brauer@moov.io>
2026-04-10 12:39:45 -0500

    feat(resolutionlinks): create resolution links status type

--- pkg/moov/resolutionlinks_models.go
+++ pkg/moov/resolutionlinks_models.go
@@ -2,17 +2,27 @@ package moov
 
 import "time"
 
+type ResolutionLinkStatus string
+
+const (
+	ResolutionLinkStatus_Active    ResolutionLinkStatus = "active"
+	ResolutionLinkStatus_Submitted ResolutionLinkStatus = "submitted"
+	ResolutionLinkStatus_Completed ResolutionLinkStatus = "completed"
+	ResolutionLinkStatus_Disabled  ResolutionLinkStatus = "disabled"
+	ResolutionLinkStatus_Expired   ResolutionLinkStatus = "expired"
+)
+
 type ResolutionLinkResponse struct {
-	ResolutionLinkCode string     `json:"code"`
-	PartnerAccountID   string     `json:"partnerAccountID"`
-	AccountID          string     `json:"accountID"`
-	CreatedOn          time.Time  `json:"createdOn"`
-	UpdatedOn          time.Time  `json:"updatedOn"`
-	ExpiresOn          time.Time  `json:"expiresOn"`
-	DisabledOn         *time.Time `json:"disabledOn,omitempty"`
-	Recipient          string     `json:"recipient"`
-	URL                string     `json:"url"`
-	Status             string     `json:"status"`
+	ResolutionLinkCode string               `json:"code"`
+	PartnerAccountID   string               `json:"partnerAccountID"`
+	AccountID          string               `json:"accountID"`
+	CreatedOn          time.Time            `json:"createdOn"`
+	UpdatedOn          time.Time            `json:"updatedOn"`
+	ExpiresOn          time.Time            `json:"expiresOn"`
+	DisabledOn         *time.Time           `json:"disabledOn,omitempty"`
+	Recipient          string               `json:"recipient"`
+	URL                string               `json:"url"`
+	Status             ResolutionLinkStatus `json:"status"`
 }
 
 type CreateResolutionLinkRequest struct {
