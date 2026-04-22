7191a3e1961042682bdab214dc310d0d8c82cf80
Carson Brown <122549408+carsonab@users.noreply.github.com>
2026-04-22 12:26:04 -0400

    Merge pull request #323 from moovfinancial/bugfix-invoice-payments
    fix(invoices): fix json tags to deserialize invoicePayments

--- pkg/moov/invoice_test.go
+++ pkg/moov/invoice_test.go
@@ -92,6 +92,12 @@ func Test_Invoice_CreateUpdateGet(t *testing.T) {
 	require.Len(t, payments, 1)
 	latestPayment := payments[0]
 	require.Equal(t, *createdPayment, latestPayment)
+
+	// Re-fetch the invoice and verify InvoicePayments
+	paidInvoice, err := mc.GetInvoice(ctx, accountID, createdInvoice.InvoiceID)
+	require.NoError(t, err)
+	require.Len(t, paidInvoice.Payments, 1)
+	require.Equal(t, createdPayment.InvoicePaymentID, paidInvoice.Payments[0].InvoicePaymentID)
 }
 
 func Test_Invoice_Delete(t *testing.T) {
