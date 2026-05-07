e1a76f77e4cb9facfeb6393634d217fef96bb057
Raphael Santo Domingo <raph@moov.io>
2026-05-07 11:13:28 -0500

    update partner account ID and source/dest pm IDs

--- examples/transfers/transfer_test.go
+++ examples/transfers/transfer_test.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"testing"
 
+	"github.com/moovfinancial/moov-go/internal/testtools"
 	"github.com/moovfinancial/moov-go/pkg/moov"
 	"github.com/moovfinancial/moov-go/pkg/mv2604"
 	"github.com/stretchr/testify/require"
@@ -16,9 +17,9 @@ func TestCreateAndPatchTransfer(t *testing.T) {
 
 	var (
 		ctx                   = context.Background()
-		partnerAccountID      = "ebbf46c6-122a-4367-bc45-7dd555e1d3b9"
-		sourcePaymentMethodID = "b9a7153a-ad9b-4ada-b31b-f230fb8d1e8b" // ach-debit-fund
-		destPaymentMethodID   = "67ebda6c-de48-474c-b49d-2cd3aa7d3f92" // moov-wallet
+		partnerAccountID      = testtools.PARTNER_ID
+		sourcePaymentMethodID = "aea716b2-3e25-42e7-9802-c75dd2dfabb1" // customer bank ach-debit-fund
+		destPaymentMethodID   = testtools.MERCHANT_WALLET_PM_ID        // moov-wallet
 		initialForeignID      = "external-ref-123"
 	)
 
