047f247a241af9d9cf7679f61fe579ffa4b21860
Vincent Xiao <vincentx72@gmail.com>
2026-09-04 14:40:14 -0700

    test(receipts): refine projection wait (PR feedback)

--- pkg/moov/receipt_test.go
+++ pkg/moov/receipt_test.go
@@ -9,70 +9,80 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
-// Tests that an async transfer becomes queryable before a receipt references it.
 func Test_Receipts(t *testing.T) {
 	mc := NewTestClient(t)
 
 	customer := CreateTemporaryTestAccount(t, mc, createTestIndividualAccount())
 	customerCard := createTemporaryCard(t, mc, customer.AccountID)
 
-	var transfer *moov.TransferStarted = nil
-	var receipt *moov.Receipt = nil
+	var transfer *moov.TransferStarted
+	var receipt *moov.Receipt
 
 	t.Run("make async transfer", func(t *testing.T) {
-		started, err := mc.CreateTransfer(BgCtx(), testtools.PARTNER_ID, moov.CreateTransfer{
-			Source: moov.CreateTransfer_Source{
-				PaymentMethodID: customerCard.PaymentMethods[0].PaymentMethodID,
-			},
-			Destination: moov.CreateTransfer_Destination{
-				PaymentMethodID: testtools.MERCHANT_WALLET_PM_ID,
-			},
-			Amount: moov.Amount{
-				Currency: "usd",
-				Value:    1,
-			},
-		}).Started()
-		NoResponseError(t, err)
-
-		// We made an async transfer, so completed should be nil, while started not nil
-		require.NotNil(t, started)
-
-		transfer = started
-		require.Eventually(t, func() bool {
-			_, err = mc.GetTransfer(BgCtx(), testtools.PARTNER_ID, transfer.TransferID)
-			return err == nil
-		}, 10*time.Second, 100*time.Millisecond)
-		require.NoError(t, err)
+		transfer = createReceiptTestTransfer(t, mc, customerCard.PaymentMethods[0].PaymentMethodID)
 	})
 
 	t.Run("create receipt", func(t *testing.T) {
-		require.NotNil(t, transfer)
-
-		receipts, err := mc.CreateReceipt(BgCtx(), moov.CreateReceipt{
-			Kind:          "sale.customer.v1",
-			ForTransferID: &transfer.TransferID,
-			Email:         moov.PtrOf("noreply@moov.io"),
-		})
-		require.NoError(t, err)
-		require.Len(t, receipts, 1)
-		require.NotEmpty(t, receipts[0].ID)
-
-		receipt = &receipts[0]
-		t.Logf("receipt: %+v\n", receipt)
+		receipt = createTestReceipt(t, mc, transfer.TransferID)
 	})
 
 	t.Run("list receipts", func(t *testing.T) {
-		require.NotNil(t, transfer)
-
-		receipts, err := mc.ListReceipts(BgCtx(), moov.ReceiptByTransferID(transfer.TransferID))
-		require.NoError(t, err)
-		require.Len(t, receipts, 1)
-
-		// The receipt may be sent or not by this point so we need to clear out the sentFor list
-		for i := range receipts {
-			receipts[i].SentFor = nil
-		}
-
-		require.Contains(t, receipts, *receipt)
+		requireTestReceiptListed(t, mc, transfer.TransferID, receipt)
 	})
+}
+
+func createReceiptTestTransfer(t *testing.T, mc *moov.Client, paymentMethodID string) *moov.TransferStarted {
+	t.Helper()
+
+	transfer, err := mc.CreateTransfer(BgCtx(), testtools.PARTNER_ID, moov.CreateTransfer{
+		Source: moov.CreateTransfer_Source{
+			PaymentMethodID: paymentMethodID,
+		},
+		Destination: moov.CreateTransfer_Destination{
+			PaymentMethodID: testtools.MERCHANT_WALLET_PM_ID,
+		},
+		Amount: moov.Amount{
+			Currency: "usd",
+			Value:    1,
+		},
+	}).Started()
+	NoResponseError(t, err)
+	require.NotNil(t, transfer)
+
+	require.Eventually(t, func() bool {
+		_, err := mc.GetTransfer(BgCtx(), testtools.PARTNER_ID, transfer.TransferID)
+		return err == nil
+	}, 3*time.Second, 250*time.Millisecond)
+
+	return transfer
+}
+
+func createTestReceipt(t *testing.T, mc *moov.Client, transferID string) *moov.Receipt {
+	t.Helper()
+
+	receipts, err := mc.CreateReceipt(BgCtx(), moov.CreateReceipt{
+		Kind:          "sale.customer.v1",
+		ForTransferID: &transferID,
+		Email:         moov.PtrOf("noreply@moov.io"),
+	})
+	require.NoError(t, err)
+	require.Len(t, receipts, 1)
+	require.NotEmpty(t, receipts[0].ID)
+
+	t.Logf("receipt: %+v\n", &receipts[0])
+	return &receipts[0]
+}
+
+func requireTestReceiptListed(t *testing.T, mc *moov.Client, transferID string, receipt *moov.Receipt) {
+	t.Helper()
+
+	receipts, err := mc.ListReceipts(BgCtx(), moov.ReceiptByTransferID(transferID))
+	require.NoError(t, err)
+	require.Len(t, receipts, 1)
+
+	for i := range receipts {
+		receipts[i].SentFor = nil
+	}
+
+	require.Contains(t, receipts, *receipt)
 }
